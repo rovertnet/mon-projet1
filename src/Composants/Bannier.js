dacfd945556a1a759e2ea955114b7d4f3de523e0
Robert Matundu <matundukabamba@gmail.com>
2022-12-10 16:48:22 +0100

    un autre commite modification insertion d'image

--- src/Composants/Bannier.js
+++ src/Composants/Bannier.js
@@ -1,8 +1,11 @@
+import logo from "../assets/logo.png"
 import "../Styles/Bannier.css"
+
 function Bannier(){
     const titre = "La Maison jungle"
     return (
         <div className='lmj-banner'>
+            <img src={logo} alt='la maison jungle' className="imj-logo"/>
             <h1 className='lmj-title'>{titre}</h1>
         </div>
     )
