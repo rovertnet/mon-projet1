3d901b9ecc8f7d8e40ed363e501e471d786ce17e
Robert Matundu <matundukabamba@gmail.com>
2022-12-11 23:39:49 +0100

    modification après pause

--- src/Composants/Sidbar.js
+++ src/Composants/Sidbar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 
 const Sidbar = () => {
   return (
-    <main className='space-toggle'>
+      <main className='space-toggle'>
           <header className='header space-toggle'>
             <div className='header-toggle'>
                 <i class="fas fa-bars"></i>
